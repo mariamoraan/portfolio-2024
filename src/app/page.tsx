8e9320032bacbdfca4c09c32b42f116299e13a43
mariamoraan <mariamoranluaces@gmail.com>
2024-03-26 21:51:25 +0100

    config: start project

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next/types";
+
+export const metadata: Metadata = {
+  title: "Acme Dashboard",
+  description: "The official Next.js Course Dashboard, built with App Router.",
+};
+
+const HomePage = () => {
+  return <main>Holaa</main>;
+};
+
+export default HomePage;
