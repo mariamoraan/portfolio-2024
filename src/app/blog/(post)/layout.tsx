213557ea90a4eae4af0c61d69eb4e766cbabd517
mariamoraan <mariamoranluaces@gmail.com>
2024-05-01 00:05:08 +0200

    feat: add metadata & change favicon

--- src/app/blog/(post)/layout.tsx
+++ src/app/blog/(post)/layout.tsx
@@ -1,6 +1,13 @@
 import { ClassNames } from "@/core/utils/class";
 import { PostHeaderComponent } from "@/libs/blog/delivery/client/post-header/post-header.component";
 import styles from "./layout.module.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "María Morán",
+  description:
+    "Soy ingeniera de software y tengo 3 años de experiencia en el desarrollo de aplicaciones web e híbridas. Me apasiona crear interfaces de usuario intuitivas, escalables y eficientes. Tengo un fuerte enfoque en la arquitectura, testing y buenas prácticas, ysiempre estoy buscando nuevas formas de mejorar la calidad del código y la experiencia del usuario.",
+};
 
 export default function PostLayout({
   children, // will be a page or nested layout
